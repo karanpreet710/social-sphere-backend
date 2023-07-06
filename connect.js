4abcd9d89dd12b61a359f9e1c0065e4142e9c852
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-06 23:02:30 +0530

    db conn change
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- connect.js
+++ connect.js
@@ -4,11 +4,16 @@ dotenv.config({
     path:'./backend/.env'
 });
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASS,
     database:process.env.DB_NAME
 })
 
-module.exports = db;
+db.getConnection((err,conn)=>{
+    if(err) console.log(err);
+    console.log("Connected successfully");
+})
+
+module.exports = db.promise();
