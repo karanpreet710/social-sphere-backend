022c8ba32d2f2922c56e6cf28c72d9a9b73b7092
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-08 00:31:37 +0530

    fixed auth error
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,14 +26,10 @@ const login = (req,res) => {
         if(!checkPassword) return res.status(400).json("Wrong password or username!");
         const token = jwt.sign({id:data[0].id},"mysecretkey");
         const {password,...others} = data[0];
-        console.log(token);
-        console.log(others)
-        console.log("Before setting the cookie");
         res.cookie("accessToken", token, {
         httpOnly: true,
         domain: ".onrender.com"
-        });
-        console.log("After setting the cookie");
+        }).status(200).json(others);
     })
 }
 
