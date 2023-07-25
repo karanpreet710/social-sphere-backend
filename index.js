dc52e322c192959810e3838948a200ae5b6e4dad
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-26 01:02:21 +0530

    changed index URL
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://socialsphere-seven.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://socialsphere-seven.onrender.com');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, *');
